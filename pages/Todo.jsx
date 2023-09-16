d525d5dbde3f7d86b96ca8f20bf54b89fa2401c9
Emmacodec <danielemmanuel7553@gmail.com>
2023-09-16 04:36:24 +0100

    update

--- pages/Todo.jsx
+++ pages/Todo.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Image from 'next/image';
-import Todo from '../public/assets/todo.jpeg';
+import To from '../public/assets/todo.jpeg';
 import {RiRadioButtonFill} from 'react-icons/ri';
 import Link from 'next/link';
 
@@ -11,7 +11,7 @@ const Todo = () => {
 
         {/*overlay*/}
         <div className='absolute top-0 left-0 w-full h-[30vh] lg:h-[40vh] bg-black/80 z-10'></div>
-        <Image className='absolute z-1' layout='fill' objectFit='cover' src={Todo} alt='/' />
+        <Image className='absolute z-1' layout='fill' objectFit='cover' src={To} alt='/' />
         <div className='absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2'>
           <h2 className='py-2'>To-do App</h2>
           <h3>React JS, Tailwind, Firebase</h3>
