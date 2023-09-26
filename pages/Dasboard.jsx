c8e2805ed0e1650a9b9526ef7f13c4b76ef3f9dd
Emmacodec <danielemmanuel7553@gmail.com>
2023-09-26 05:05:34 +0100

    update

--- pages/Dasboard.jsx
+++ pages/Dasboard.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Image from 'next/image';
-import Dash from '../public/assets/dashboard.jpeg';
+import Dash from '../public/assets/coin.jpeg';
 import {RiRadioButtonFill} from 'react-icons/ri';
 import Link from 'next/link';
 
@@ -13,8 +13,8 @@ const Dashboard = () => {
         <div className='absolute top-0 left-0 w-full h-[30vh] lg:h-[40vh] bg-black/80 z-10'></div>
         <Image className='absolute z-1' layout='fill' objectFit='cover' src={Dash} alt='/' />
         <div className='absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2'>
-          <h2 className='py-2'>User Dashboard UI</h2>
-          <h3>Next JS, Tailwind, JavaScript</h3>
+          <h2 className='py-2'>Crypto Currency Dashboard</h2>
+          <h3>React JS, BootStrap, JavaScript</h3>
         </div>
       </div>
 
@@ -23,14 +23,12 @@ const Dashboard = () => {
         <div className='col-span-4'>
           <p className='tracking-widest uppercase text-[#5651e5] text-xl'>Project</p>
           <h2>Overview</h2>
-          <p>This web app was built using React JS and hosted on Netlify. Users are able to select their favorite
-            holiday resort for friends, family and loved ones. I use the React useState hook to toggle my mobile
-            responsive navbar state and build a custom image component.
+          <p>Built a multi-page React JS application centered on cryptocurrencies. Used Axios to make the API call from the coingeckoapi.com, also used DOMpurify to to prevent cross-tag scripting, used useParam hook to pass data as parameters.
           </p>
-          <a href='https://emmy-dan.netlify.app'>
+          <a href=''>
           <button className='px-8 py-2 mt-4 mr-8'>Demo</button>
           </a>
-          <a href='http://github.com/emmacodec/dashboard-app/'>
+          <a href='http://github.com/emmacodec/antwaspoes'>
           <button className='px-8 py-2 mt-4'>Code</button>
           </a>
         </div>
@@ -39,8 +37,10 @@ const Dashboard = () => {
             <p className='text-center font-bold pb-2'>Technologies</p>
             <div className='grid grid-cols-3 md:grid-cols-1'>
               <p className='text-gray-600 py-2 flex items-center'><RiRadioButtonFill className='pr-1' /> React</p>
-              <p className='text-gray-600 py-2 flex items-center'><RiRadioButtonFill className='pr-1' /> Tailwind</p>
+              <p className='text-gray-600 py-2 flex items-center'><RiRadioButtonFill className='pr-1' /> Bootstrap</p>
               <p className='text-gray-600 py-2 flex items-center'><RiRadioButtonFill className='pr-1' /> JavaScript</p>
+              <p className='text-gray-600 py-2 flex items-center'><RiRadioButtonFill className='pr-1' /> coin Gecko API</p>
+              <p className='text-gray-600 py-2 flex items-center'><RiRadioButtonFill className='pr-1' /> Router</p>
          </div>
           </div>
         </div>
