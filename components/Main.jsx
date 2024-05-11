611dfa3b91546b3bc6113be5ab8275ce4eaf1ff3
Emmacodec <danielemmanuel7553@gmail.com>
2024-05-11 09:09:22 +0100

    update

--- components/Main.jsx
+++ components/Main.jsx
@@ -30,7 +30,7 @@ const Main = () => {
             for new challenges that allow me to continue growing professionally.
           </p>
 
-          <a href="https://drive.google.com/file/d/1uuAgCpFRAzLzoH04T4qzsVzGwWI2GIKU/view?usp=drivesdk">
+          <a href="https://drive.google.com/file/d/1ymcpHZ0p7r7aS1RBnwgDVhowPkPrUzRh/view?usp=drive_link">
             <button className='px-8 py-2 mt-4 mr-8'>Resume</button>
           </a>
 
